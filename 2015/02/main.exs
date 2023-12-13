8ffb0af44a984f301b15bb011d1c7d94154e08d6
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2023-12-13 07:52:56 -0300

    feat: aoc 2015 day 2 puzzle 2

--- 2015/02/main.exs
+++ 2015/02/main.exs
@@ -5,23 +5,31 @@ defmodule Solution do
 
     contents
     |> String.split("\n")
-    |> Enum.reduce(0, fn dimension, acc ->
+    |> Enum.reduce([0, 0], fn dimension, [box_area, ribbon_area] ->
       dimension
       |> String.split("x")
       |> Enum.map(&String.to_integer/1)
       |> Enum.sort()
       |> calculate_area()
-      |> Kernel.+(acc)
+      |> sum([box_area, ribbon_area])
     end)
     |> IO.inspect()
   end
 
   # [l, w, h]
   def calculate_area(dimensions) do
-    2 * Enum.at(dimensions, 0) * Enum.at(dimensions, 1) +
-      2 * Enum.at(dimensions, 1) * Enum.at(dimensions, 2) +
-      2 * Enum.at(dimensions, 0) * Enum.at(dimensions, 2) +
-      Enum.reduce(Enum.take(dimensions, 2), &(&1 * &2))
+    [
+      2 * Enum.at(dimensions, 0) * Enum.at(dimensions, 1) +
+        2 * Enum.at(dimensions, 1) * Enum.at(dimensions, 2) +
+        2 * Enum.at(dimensions, 0) * Enum.at(dimensions, 2) +
+        Enum.reduce(Enum.take(dimensions, 2), &(&1 * &2)),
+      Enum.reduce(Enum.take(dimensions, 2), &((&1 + &2) * 2)) +
+        Enum.reduce(dimensions, &(&1 * &2))
+    ]
+  end
+
+  def sum(new_areas, acc) do
+    [Enum.at(new_areas, 0) + Enum.at(acc, 0), Enum.at(new_areas, 1) + Enum.at(acc, 1)]
   end
 end
 
