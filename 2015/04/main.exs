45805d2f2d3d57e133438b9ae3b0d848857fb4f9
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2024-08-20 16:52:30 -0300

    feat: aoc 2015 day 4 puzzle 2

--- 2015/04/main.exs
+++ 2015/04/main.exs
@@ -1,16 +1,17 @@
 defmodule Solution do
   def solve do
-    IO.puts("Puzzle 1: #{mine_puzzle_1("yzbqklnj", 0)}")
+    IO.puts("Puzzle 1: #{mine("yzbqklnj", 0, 5)}")
+    IO.puts("Puzzle 2: #{mine("yzbqklnj", 0, 6)}")
   end
 
-  def mine_puzzle_1(input, index) do
+  def mine(input, index, zeros_count) do
     if :crypto.hash(:md5, input <> Integer.to_string(index))
        |> Base.encode16()
-       |> String.slice(0, 5) ==
-         "00000" do
+       |> String.slice(0, zeros_count) ==
+         String.duplicate("0", zeros_count) do
       index
     else
-      mine_puzzle_1(input, index + 1)
+      mine(input, index + 1, zeros_count)
     end
   end
 end
